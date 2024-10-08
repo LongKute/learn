d7d291de363ff4e7078e07b5281abfb205cf1281
Xuân hạ <113323575+LongKute@users.noreply.github.com>
2024-10-08 14:21:39 +0700

    v1.1
    add Js.doc

--- learn/1.js
+++ learn/1.js
@@ -1,29 +1,4 @@
-// const arr1 = [1,2,3,4,5,6,7]
-// const ob = {
-//     name: "Long_kute",
-//     age: 24,
-// }
 
-// function test(ob) {
-
-//     // let newa = [ob];
-//     // newa.push(({a}) => {
-//     //     for (let i = 0; i < a.length; i++) {
-//     //         if (a[i] %2 == 0) {
-              
-                
-//     //         }
-            
-//     //     }
-//     // } )
-//     // console.log(newa)
-//     return { ...ob, arr: arr1.filter(item => item % 2 === 0) };
-// }
-
-// console.log(test(ob))
-
-// const cloneUser = [...user]
-// console.log(cloneUser);
 const user = {
   name: 'duoc',
   age: 24,
