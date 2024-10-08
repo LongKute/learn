eeeb993e96832895671e668b7f4c294efbeec346
Longkute <113323575+LongKute@users.noreply.github.com>
2024-10-08 22:56:56 +0700

    v1.4
    dang lam den bai 23 bai tap cua thua

--- baitapThua/22.js
+++ baitapThua/22.js
@@ -1,1 +1,8 @@
 //22. Tạo một hàm đệ quy để tính dãy Fibonacci tại vị trí n.
+function fibonacci(num) {
+  if (num <= 1) return num;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(10));
